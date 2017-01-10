6914f7e9b5c4b0571ca72a192fc3757e4fe461dd
Erik Tollerud <erik.tollerud@gmail.com>
2017-01-10 15:11:50 -0500

    more updates

--- decals.py
+++ decals.py
@@ -19,7 +19,8 @@ def make_cutout_comparison_table(dcat, dhtml=True, doprint=True):
         dviewurl = 'http://legacysurvey.org/viewer?ra={}&dec={}'.format(row['ra'], row['dec'])
         sviewurl = 'http://skyserver.sdss.org/dr12/en/tools/chart/navi.aspx?ra={}&dec={}'.format(row['ra'], row['dec'])
         sc = SkyCoord(row['ra'], row['dec'], unit=u.deg)
-        objstr = '{}_{}<br>RA={:.4f}<br>Dec={:.4f}<br>r={:.2f}<br>sb={:.2f}'.format(row['brickid'], row['objid'], row['ra'], row['dec'], row['r'], row['sb_r_0.5'])
+        objstr = '{}_{}<br>RA={:.4f}<br>Dec={:.4f}<br>r={:.2f}<br>sb={:.2f}<br>anymsk={}<br>allmsk={}'.format(row['brickid'],
+            row['objid'], row['ra'], row['dec'], row['r'], row['sb_r_0.5'], row['decam_anymask'], row['decam_allmask'])
         deimg = '<a href="{}"><img src="{}"></a>'.format(dviewurl, de_cutout_url.format(sc))
         sdimg = '<a href="{}"><img src="{}"></a>'.format(sviewurl, sd_cutout_url.format(sc))
         tabrows.append('<tr><td>{}</td><td>{}</td><td>{}</td></tr>'.format(objstr, deimg, sdimg))
